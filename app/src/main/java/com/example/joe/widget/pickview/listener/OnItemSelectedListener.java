33434321ad9155c0c076fee04897807390d77478
qiaobing <joeqiaobing@sina.cn>
2017-12-28 17:27:46 +0800

    时间或城市选择器

---
+++ app/src/main/java/com/example/joe/widget/pickview/listener/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.example.joe.widget.pickview.listener;
+
+
+public interface OnItemSelectedListener {
+    void onItemSelected(int index);
+}
---
+++ app/src/main/java/com/example/joe/widget/pickview/model/IPickerViewData.java
@@ -0,0 +1,8 @@
+package com.example.joe.widget.pickview.model;
+
+/**
+ * Created by Sai on 2016/7/13.
+ */
+public interface IPickerViewData {
+    String getPickerViewText();
+}
